bb513c40cc51d81c05522c05cae64eaa42f34af9
dapoade <69828169+dapoade@users.noreply.github.com>
2023-07-27 01:02:30 -0500

    pick and roll defense
    - pick and roll defense code 
    - code that I used to generate the stream lit app and and generate plots and tables for my report can be found here

---
+++ picks.py
@@ -0,0 +1,318 @@
+import pandas as pd
+import numpy as np 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+df = pd.read_csv("z_Picks_22_23.csv")
+
+# filter for when the person guarding the screen is a 4 or 5
+
+bigs = (df.query("Position_NBA_SCR_D == 4 or Position_NBA_SCR_D == 5")[[
+    "GameDate"
+    , "SeasonType"
+    , "ChanceId"
+    , "Period"
+    , "Team_Off"
+    , "Team_Def"
+    , "Player_BHR_O"
+    , "Position_NBA_BHR_O"
+    , "Player_BHR_D"
+    , "Position_NBA_BHR_D"
+    , "Player_SCR_O"
+    , "Position_NBA_SCR_O"
+    , "Player_SCR_D"
+    , "Position_NBA_SCR_D"
+    , "IsDirect"
+    , "CoverageType_BHR_D"
+    , "CoverageType_SCR_D"
+    , "ChancePoints"
+
+]]) 
+
+# get dummy variables for screener defense type 
+
+big_defenders = pd.get_dummies(bigs,
+                columns = ['CoverageType_SCR_D'])
+
+# rename columns 
+big_defenders.rename(columns={
+    'CoverageType_SCR_D_blitz': 'blitz',
+    'CoverageType_SCR_D_ice': 'ice',
+    'CoverageType_SCR_D_none': 'none',
+    'CoverageType_SCR_D_show': 'show',
+    'CoverageType_SCR_D_soft': 'soft',
+    'CoverageType_SCR_D_switch': 'switch',
+}, inplace=True) 
+
+# get versatility rating by defender
+def get_versatile_defenders(df: df = big_defenders, season : str = 'RS'):
+
+    """
+    Function that returns a dataframe of Screener defenders with percent of time spent in each coverage,
+    their PPDA allowed, and their versatility rating. 
+
+    Args:
+        - df: original dataframe of each pick and roll 
+        - season: regular season or playoffs 
+    """
+
+    versatile_defenders = (df
+                       .query("IsDirect == 1")
+                       .groupby(['SeasonType', 'Player_SCR_D'])
+                       .apply(lambda x: pd.DataFrame({
+                           'screens_guarded' : [x.shape[0]],
+                           'blitz' : x['blitz'].sum() / len(x),
+                           'ice' : x['ice'].sum() / len(x),
+                           'none' : x['none'].sum() / len(x),
+                           'show' : x['show'].sum() / len(x),
+                           'soft' : x['soft'].sum() / len(x),
+                           'switch' : x['switch'].sum() / len(x),
+                            'PPDA' : x['ChancePoints'].sum() / len(x) })
+                       ) ).reset_index().drop('level_2', axis=1)
+    
+    # calculate versatility index, drop level 2 column, 
+    versatile_defenders['versatility'] = (versatile_defenders[['blitz',
+                                                'ice',
+                                                'none',
+                                                'show',
+                                                'soft',
+                                                'switch']].apply(lambda row: (row ** 2).sum(), axis = 1))
+    
+    versatile_defenders['versatility'] = 1 - versatile_defenders['versatility']
+
+    defenders = versatile_defenders.query(f"SeasonType == '{season}' and screens_guarded >= 40").sort_values(by = 'versatility', ascending = False)
+
+    # calculate rank for 
+    defenders['ppda_Rk'] = round(100 - defenders['PPDA'].rank(ascending = True, method = 'min', pct = True) * 100, 0).astype('int')
+    defenders['vers_Rk'] = round(100 - defenders['versatility'].rank(ascending = False, method = 'min', pct = True) * 100, 0).astype('int')
+
+    return defenders
+
+
+
+# defenders = get_versatile_defenders(big_defenders, 'PO')
+
+
+# plot defender versatility vs ppda allowed 
+def plot_defenders(df: df):
+
+    x = df['versatility']
+    y = df['PPDA']
+
+    ax = sns.regplot(x = x, y =  y, scatter_kws={"color": "black"}, line_kws={"color": "red"}, ci = None )
+    ax.set(xlabel = 'Versatility Rating by Player', ylabel= 'Points per Direct Attempt Allowed', title= f"Player Versatility vs Effectiveness -- {df['SeasonType'].iloc[0]} ")
+    
+    return ax
+
+
+# plot_defenders(defenders)
+
+
+# calculate  versatility rating by team
+def get_versatile_teams(df: df = big_defenders, season: str = 'RS'):
+    """
+    Function that returns a dataframe of teams with percent of time spent in each coverage,
+    their PPDA allowed, and their versatility rating. 
+
+    Args:
+        - df: original dataframe of each pick and roll 
+        - season: regular season or playoffs 
+    """
+
+    versatile_teams = (df
+                       .query("IsDirect == 1")
+                       .groupby(['SeasonType', 'Team_Def'])
+                       .apply(lambda x: pd.DataFrame({
+                           'screens_guarded' : [x.shape[0]],
+                           'blitz' : x['blitz'].sum() / len(x),
+                           'ice' : x['ice'].sum() / len(x),
+                           'none' : x['none'].sum() / len(x),
+                           'show' : x['show'].sum() / len(x),
+                           'soft' : x['soft'].sum() / len(x),
+                           'switch' : x['switch'].sum() / len(x),
+                           'PPDA' : x['ChancePoints'].sum() / len(x) })) 
+                            ).reset_index().drop('level_2', axis=1)
+
+    versatile_teams['versatility'] = (versatile_teams[['blitz',
+                                                    'ice',
+                                                    'none',
+                                                    'show',
+                                                    'soft',
+                                                    'switch']].apply(lambda row: (row ** 2).sum(), axis = 1))
+
+    versatile_teams['versatility'] = 1 - versatile_teams['versatility']
+
+    teams = versatile_teams.query(f"SeasonType == '{season}' ").sort_values(by = 'versatility', ascending = False)
+
+    teams['ppda_Rk'] = round(teams['PPDA'].rank(ascending = True, method = 'min'), 0).astype('int')
+    teams['vers_Rk'] = round(teams['versatility'].rank(ascending = False, method = 'min'), 0).astype('int')
+
+    return teams 
+
+
+
+# teams = get_versatile_teams(big_defenders, 'PO')
+
+# plot team versatility vs ppda allowed 
+def plot_teams(df: df):
+
+    x = df['versatility']
+    y = df['PPDA']
+
+    ax = sns.regplot(x = x, y =  y, scatter_kws={"color": "black"}, line_kws={"color": "red"}, ci = None )
+    ax.set(xlabel = 'Versatility Rating by Team', ylabel= 'Points per Direct Attempt Allowed', title= f"Team Versatility vs Effectiveness -- {df['SeasonType'].iloc[0]}")
+
+    return ax 
+
+# plot_teams(teams)
+
+
+
+# get versatility by team per game 
+
+def get_game_versatility(df: df = big_defenders, season: str = 'RS'):
+
+    game_agg = (df
+        .query("IsDirect == 1 ")
+        .groupby(['SeasonType', 'Team_Def', 'GameDate'])
+        .apply(lambda x: pd.DataFrame({
+            'screens_guarded' : [x.shape[0]],
+            'blitz' : x['blitz'].sum() / len(x),
+            'ice' : x['ice'].sum() / len(x),
+            'none' : x['none'].sum() / len(x),
+            'show' : x['show'].sum() / len(x),
+            'soft' : x['soft'].sum() / len(x),
+            'switch' : x['switch'].sum() / len(x),
+            'PPDA' : x['ChancePoints'].sum() / len(x) })) 
+        .reset_index()
+        )
+
+    # versatility rating by game, based on coverage type 
+    game_agg['versatility'] = (game_agg[['blitz',
+                                        'ice',
+                                        'none',
+                                        'show',
+                                        'soft',
+                                        'switch']].apply(lambda row: (row ** 2).sum(), axis = 1))
+    # make higher versatility better by subtracting 1 
+    game_agg['versatility'] = 1 - game_agg['versatility']
+
+    final_df = game_agg.query(f"SeasonType == '{season}' ")
+
+    return final_df
+
+# game_versatility = get_game_versatility(big_defenders, 'RS')
+
+# plot versatility by team per game vs ppda allowed
+
+def plot_game_versatility(df: df):
+
+    x = df['versatility']
+    y = df['PPDA']
+
+    ax = sns.regplot(x = x, y = y, scatter_kws={"color": "black"}, line_kws={"color": "red"} )
+    ax.set(xlabel = 'Versatility Rating by Game', ylabel= 'Points per Direct Attempt Allowed', title= f"Versatility vs Effectiveness -- {df['SeasonType'].iloc[0]} ")
+
+    return ax 
+
+# plot_game_versatility(game_versatility)
+
+
+def coverage_by_quarter(df: df = big_defenders, season : str = 'RS'):
+
+    """Returns league level pick and roll coverage frequency and points per direct attempt allowed by season tyoe"""
+
+    vers_quarter = (df
+    .query("IsDirect == 1 ")
+    .groupby(['SeasonType', 'Period' ])
+    .apply(lambda x: pd.DataFrame({
+        'screens_guarded' : [x.shape[0]],
+        'blitz' : x['blitz'].sum() / len(x),
+        'ice' : x['ice'].sum() / len(x),
+        'none' : x['none'].sum() / len(x),
+        'show' : x['show'].sum() / len(x),
+        'soft' : x['soft'].sum() / len(x),
+        'switch' : x['switch'].sum() / len(x),
+        'PPDA' : x['ChancePoints'].sum() / len(x) })) 
+    .reset_index().drop('level_2', axis = 1)
+    )
+
+    vers_quarter = vers_quarter.query(f"SeasonType == '{season}' and screens_guarded > 500 ")
+
+    return vers_quarter
+
+def league_wide_coverages(df: df = bigs):
+    """ Returns data frame of the frequency and points allowed for each coverage on the league level"""
+
+    coverage_types = (bigs
+        .query("IsDirect == 1")
+        .groupby(['SeasonType', 'CoverageType_SCR_D'])
+        .apply(lambda x: pd.DataFrame({
+            'PPDA Allowed' : [x['ChancePoints'].sum() / len(x)],
+        }))
+        ).reset_index().drop('level_2', axis = 1)
+
+    freq = bigs.query("IsDirect == 1").groupby(['SeasonType', 'CoverageType_SCR_D']).size().reset_index(name='Frequency')
+    freq['Freq'] = round((freq['Frequency'] / freq.groupby('SeasonType')['Frequency'].transform('sum')) * 100, 0)
+    freq = freq.drop(columns=['Frequency'])
+
+    coverages = pd.merge(freq, coverage_types, on = ['SeasonType', 'CoverageType_SCR_D'])
+    final_coverages = coverages.set_index(['CoverageType_SCR_D', 'SeasonType']).sort_index(ascending = False)
+    # final_coverages['Freq'] = final_coverages['Freq'] + '%'
+
+    return final_coverages
+
+def player_team_coverages(df: df = bigs, season: str = 'RS', team: str = 'MEM'):
+
+    """ Returns a dataframe of pick and roll coverage type frequency and points per direct attempt allowed by player on a specified team"""
+
+    coverage_types = (df
+            .query(f"IsDirect == 1 and Team_Def == '{team}' and SeasonType == '{season}'")
+            .groupby(['SeasonType', 'CoverageType_SCR_D', 'Player_SCR_D'])
+            .apply(lambda x: pd.DataFrame({
+                'Screens Guarded' : [x.shape[0]],
+                'PPDA Allowed' : [x['ChancePoints'].sum() / len(x)],
+            }))
+            ).reset_index().drop('level_3', axis = 1 )
+    
+    coverage_types['Frequency'] = round(coverage_types['Screens Guarded'] / coverage_types.groupby(['SeasonType', 'Player_SCR_D'])['Screens Guarded'].transform('sum') * 100, 0)
+    coverage_types_final = coverage_types[['Player_SCR_D', 'CoverageType_SCR_D', 'Screens Guarded','Frequency', 'PPDA Allowed']]
+
+    return coverage_types_final
+
+
+def team_coverages(df: df = bigs, season: str = 'RS', team: str = 'MEM'):
+
+    """ Returns a dataframe of pick and roll coverage type frequency and points per direct attempt allowed by a specified team"""
+
+    coverage_types = (df
+            .query(f"IsDirect == 1 and Team_Def == '{team}' and SeasonType == '{season}'")
+            .groupby(['SeasonType', 'CoverageType_SCR_D'])
+            .apply(lambda x: pd.DataFrame({
+                'Screens Guarded' : [x.shape[0]],
+                'PPDA Allowed' : [x['ChancePoints'].sum() / len(x)],
+            }))
+            ).reset_index().drop('level_2', axis = 1 )
+    
+    coverage_types['Frequency'] = round(coverage_types['Screens Guarded'] / coverage_types.groupby(['SeasonType'])['Screens Guarded'].transform('sum') * 100, 0)
+    coverage_types_final = coverage_types[['CoverageType_SCR_D', 'Screens Guarded','Frequency', 'PPDA Allowed']]
+
+    return coverage_types_final
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
---
+++ streamlit_app.py
@@ -0,0 +1,208 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from picks import coverage_by_quarter, get_game_versatility, get_versatile_teams, get_versatile_defenders, league_wide_coverages, player_team_coverages, team_coverages, bigs 
+# import z_picks
+
+# coverage_by_quarter(season = 'PO')
+st.set_page_config(layout="wide")
+
+
+def page_one():
+
+    st.title("Pick and Roll Defensive Versatility")
+    st.title("Around the League")
+
+    # team table
+
+    rs_team = get_versatile_teams(season = 'RS')[['Team_Def', 'screens_guarded', 'PPDA', 'ppda_Rk', 'versatility', 'vers_Rk']].reset_index(drop= True).set_index('Team_Def')
+    po_team = get_versatile_teams(season = 'PO')[['Team_Def', 'screens_guarded', 'PPDA', 'ppda_Rk', 'versatility', 'vers_Rk']].reset_index(drop= True).set_index('Team_Def')
+
+
+
+    # player table 
+
+    rs_defenders = get_versatile_defenders(season = 'RS')[['Player_SCR_D', 'screens_guarded', 'PPDA', 'ppda_Rk', 'versatility', 'vers_Rk']].reset_index(drop= True).set_index('Player_SCR_D')
+    po_defenders = get_versatile_defenders(season = 'PO')[['Player_SCR_D', 'screens_guarded', 'PPDA', 'ppda_Rk', 'versatility', 'vers_Rk']].reset_index(drop= True).set_index('Player_SCR_D')
+
+
+    table_selection_team = st.radio("Select Season:", ('Regular Season', 'Playoffs'))
+
+    if table_selection_team == 'Regular Season':
+            
+
+            st.header('Team Info')
+            rs_team = rs_team.style.background_gradient(subset = ['ppda_Rk', 'vers_Rk'], cmap= 'RdYlGn_r')
+            rs_team = rs_team.format(precision = 3)
+            st.dataframe(rs_team, width = 2000, column_config = {'PPDA' : 'PPDA Allowed',
+                                                                'ppda_Rk': 'PPDA Rank',
+                                                                'screens_guarded':'Screens Guarded',
+                                                                'Team_Def': 'Def. Team',
+                                                                'versatility': 'Versatility',
+                                                                'vers_Rk': 'Versatility Rank' } )
+
+
+            st.header('Player Info')
+
+            rs_defenders = rs_defenders.style.background_gradient(subset = ['ppda_Rk', 'vers_Rk'], cmap= 'RdYlGn')
+            rs_defenders = rs_defenders.format(precision = 3)
+            st.dataframe(rs_defenders, width = 2000, column_config = {'PPDA' : 'PPDA Allowed',
+                                                                      'Player_SCR_D': 'Screener Defender',
+                                                                      'screens_guarded':'Screens Guarded',
+                                                                      'ppda_Rk' : 'PPDA Percentile Rank',
+                                                                      'versatility' : 'Versatility',
+                                                                      'vers_Rk': 'Versatilty Percentile Rank'} )
+        
+    elif table_selection_team == 'Playoffs':
+            
+            st.header('Team Info')
+
+            po_team = po_team.style.background_gradient(subset = ['ppda_Rk', 'vers_Rk'], cmap= 'RdYlGn_r')
+            po_team = po_team.format(precision = 3)
+            st.dataframe(po_team, width = 2000, column_config = {'PPDA' : 'PPDA Allowed',
+                                                                'ppda_Rk': 'PPDA Rank',
+                                                                'screens_guarded':'Screens Guarded',
+                                                                'Team_Def': 'Def. Team',
+                                                                'versatility': 'Versatility',
+                                                                'vers_Rk': 'Versatility Rank' } )
+            
+            st.header('Player Info')
+
+            po_defenders = po_defenders.style.background_gradient(subset = ['ppda_Rk', 'vers_Rk'], cmap= 'RdYlGn')
+            po_defenders = po_defenders.format(precision = 3)
+            st.dataframe(po_defenders, width = 2000, column_config = {'PPDA' : 'PPDA Allowed',
+                                                                      'Player_SCR_D': 'Screener Defender',
+                                                                      'screens_guarded':'Screens Guarded',
+                                                                      'ppda_Rk' : 'PPDA Percentile Rank',
+                                                                      'versatility' : 'Versatility',
+                                                                      'vers_Rk': 'Versatilty Percentile Rank'} )
+
+    st.header("League Level Coverages")
+    coverages = league_wide_coverages()
+    coverages['Freq'] = coverages['Freq'].astype(str) + '%'
+    coverages = coverages.style.background_gradient(subset=['PPDA Allowed'], cmap='RdYlGn_r')
+    coverages.format(precision = 3)
+    st.dataframe(coverages, column_config = {'CoverageType_SCR_D': ''})
+
+def page_two():
+
+    
+    nba_teams = {
+    'ATL': 'Atlanta Hawks',
+    'BKN': 'Brooklyn Nets',
+    'BOS': 'Boston Celtics',
+    'CHA': 'Charlotte Hornets',
+    'CHI': 'Chicago Bulls',
+    'CLE': 'Cleveland Cavaliers',
+    'DAL': 'Dallas Mavericks',
+    'DEN': 'Denver Nuggets',
+    'DET': 'Detroit Pistons',
+    'GSW': 'Golden State Warriors',
+    'HOU': 'Houston Rockets',
+    'IND': 'Indiana Pacers',
+    'LAC': 'LA Clippers',
+    'LAL': 'Los Angeles Lakers',
+    'MEM': 'Memphis Grizzlies',
+    'MIA': 'Miami Heat',
+    'MIL': 'Milwaukee Bucks',
+    'MIN': 'Minnesota Timberwolves',
+    'NOP': 'New Orleans Pelicans',
+    'NYK': 'New York Knicks',
+    'OKC': 'Oklahoma City Thunder',
+    'ORL': 'Orlando Magic',
+    'PHI': 'Philadelphia 76ers',
+    'PHX': 'Phoenix Suns',
+    'POR': 'Portland Trail Blazers',
+    'SAC': 'Sacramento Kings',
+    'SAS': 'San Antonio Spurs',
+    'TOR': 'Toronto Raptors',
+    'UTA': 'Utah Jazz',
+    'WAS': 'Washington Wizards'
+                                }
+    
+    selected_team = st.sidebar.selectbox('Select an NBA Team', list(nba_teams.keys()))
+
+    st.title(f'{nba_teams[selected_team]} Team Page')
+
+
+    st.header("Pick and Roll Coverages by Player")
+
+    selected_season = st.radio("Select Season:", ('Regular Season', 'Playoffs'))
+
+    if selected_season == "Playoffs":
+
+
+        st.header("Pick and Roll Coverages")
+
+        teams = team_coverages(season = 'PO', team = selected_team)
+        teams['Frequency'] = teams['Frequency'].astype(str) + '%'
+        teams = teams[['CoverageType_SCR_D', 'Screens Guarded', 'Frequency', 'PPDA Allowed']]
+        teams = teams.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r').format(precision = 3)
+
+        st.dataframe(teams, hide_index = True, column_config = {'CoverageType_SCR_D' : 'Coverage'})
+
+        player_search_query = st.text_input("Search by Player:", "")
+        coverage_type_search_query = st.text_input("Search by Coverage:", "")
+
+        coverage_by_team = player_team_coverages(season = 'PO', team = selected_team)
+        coverage_by_team['Frequency'] = coverage_by_team['Frequency'].astype(str) + '%'
+        
+        # coverage_by_team = coverage_by_team.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r')
+
+        # Filter the DataFrame based on the search query
+        coverage_by_team = coverage_by_team[
+                                            (coverage_by_team['Player_SCR_D'].str.contains(player_search_query, case=False)) &
+                                            (coverage_by_team['CoverageType_SCR_D'].str.contains(coverage_type_search_query, case=False))
+                                            ]
+        coverage_by_team = coverage_by_team.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r')
+        st.dataframe(coverage_by_team, hide_index = True, column_config = {'Player_SCR_D': 'Def. Player',
+                                                        'CoverageType_SCR_D' : 'Coverage'})
+        
+    
+    elif selected_season == 'Regular Season':
+
+
+        st.header("Pick and Roll Coverages")
+
+        teams = team_coverages(season = 'RS', team = selected_team)
+        teams['Frequency'] = teams['Frequency'].astype(str) + '%'
+        teams = teams[['CoverageType_SCR_D', 'Screens Guarded', 'Frequency', 'PPDA Allowed']]
+        teams = teams.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r').format(precision = 3)
+
+        st.dataframe(teams, hide_index = True, column_config = {'CoverageType_SCR_D' : 'Coverage'})
+
+        player_search_query = st.text_input("Search by Player:", "")
+        coverage_type_search_query = st.text_input("Search by Coverage:", "")
+         
+        coverage_by_team = player_team_coverages(season = 'RS', team = selected_team)
+        coverage_by_team['Frequency'] = coverage_by_team['Frequency'].astype(str) + '%'
+        
+        # coverage_by_team = coverage_by_team.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r')
+
+        # Filter the DataFrame based on the search query
+        coverage_by_team = coverage_by_team[
+                                            (coverage_by_team['Player_SCR_D'].str.contains(player_search_query, case=False)) &
+                                            (coverage_by_team['CoverageType_SCR_D'].str.contains(coverage_type_search_query, case=False))
+                                            ]
+        coverage_by_team = coverage_by_team.style.background_gradient(subset = 'PPDA Allowed', cmap= 'RdYlGn_r').format(precision = 3)
+        st.dataframe(coverage_by_team, hide_index = True, column_config = {'Player_SCR_D': 'Def. Player',
+                                                        'CoverageType_SCR_D' : 'Coverage'})
+        
+
+
+st.sidebar.title('Pages:')
+selection = st.sidebar.selectbox("Go to:", ("League Page", "Team Page"))
+
+if selection == 'League Page':
+    page_one()
+elif selection == 'Team Page':
+    page_two()
+
+st.sidebar.caption(" *All pick and roll information is calculated using direct pick and rolls*")
+
+
+
+
+
